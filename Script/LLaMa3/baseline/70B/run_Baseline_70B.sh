e5def2154babad37110234e00d84e2673648ac8a
chl <rimi11e@hyolim112@gmail.com>
2024-12-12 17:27:32 +0900

    LLaMa_Script

---
+++ Script/LLaMa3/baseline/70B/run_Baseline_70B.sh
@@ -0,0 +1,4 @@
+python3.10 /home/hhs/main/llama3/chr/baseline/70B/BaseLine_70B.py \
+    --input-path /home/hhs/main/llama3/chr/Data/original_input.json \
+    --example-path /home/hhs/main/llama3/chr/baseline/8B/baseline_prompt.txt \
+    --output-path ./Full_Baseline_70B.json\
---
+++ Script/LLaMa3/baseline/8B/run_Baseline.sh
@@ -0,0 +1,4 @@
+python3.10 /home/hhs/main/llama3/chr/baseline/8B/BaseLine.py \
+    --input-path /home/hhs/main/llama3/chr/Data/original_input.json \
+    --example-path /home/hhs/main/llama3/chr/baseline/8B/baseline_prompt.txt \
+    --output-path ./22222222_Baseline.json\
