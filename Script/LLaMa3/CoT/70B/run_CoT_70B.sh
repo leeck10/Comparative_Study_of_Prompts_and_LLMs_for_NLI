cfa24d85b4b8d6254cb4157d8b44bac5c34eda05
chl <rimi11e@hyolim112@gmail.com>
2024-12-12 17:28:13 +0900

    LLaMa_baseline

---
+++ Script/LLaMa3/CoT/70B/run_CoT_70B.sh
@@ -0,0 +1,4 @@
+python3.10 /home/hhs/main/llama3/chr/CoT/70B/CoT_70B_Full.py \
+    --input-path /home/hhs/main/llama3/chr/Data/original_input.json \
+    --example-path /home/hhs/main/llama3/chr/CoT/8B/CoT_prompt.txt \
+    --output-path ./Full_CoT_70B.json\
---
+++ Script/LLaMa3/CoT/8B/run_CoT.sh
@@ -0,0 +1,4 @@
+python3.10 /home/hhs/main/llama3/chr/CoT/8B/CoT.py \
+    --input-path /home/hhs/main/llama3/chr/Data/original_input.json \
+    --example-path /home/hhs/main/llama3/chr/CoT/8B/CoT_prompt.txt \
+    --output-path ./222222222_CoT.json\
