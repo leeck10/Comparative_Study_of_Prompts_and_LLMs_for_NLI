e1f05bbf59511c942cb7dda9ad3dbd44776389c2
chl <rimi11e@hyolim112@gmail.com>
2024-12-12 17:34:48 +0900

    Phi3_Script_CoT

---
+++ Script/Phi3/CoT/0shot/phi3_mini_CoT.py
@@ -0,0 +1,85 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+import torch
+import argparse
+import json
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+model_id = "microsoft/Phi-3-mini-4k-instruct"
+#model_id = "microsoft/Phi-3-mini-128k-instruct"
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_id, 
+    device_map="cuda:1", 
+    torch_dtype="auto", 
+    trust_remote_code=True, 
+)
+pipe = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+)
+generation_args = {
+    "max_new_tokens": 500,
+    "return_full_text": False,
+    "temperature": 0.0,
+    "do_sample": False,
+}
+
+def response(args, premise, hypothesis):
+    with open(args.example_path) as f:
+        example_string = f.read().split('{shot}')[:(args.shot)]
+    if args.shot > 0:
+        example = ''.join(example_string)
+    prefix = "Infer that the relationship between premises and hypothesis is ‘contradiction’, ‘entailment’, or ‘neutral’. Let's think step by step. Explain the reason for your inference and at the end, answer with one of the following: 'contradiction', 'entailment', or 'neutral'."
+    
+    messages = [
+        #{"role": "system", "content": f'{prefix}'},
+        {"role": "user", "content": f'{prefix}\npremise:{premise}\nhypothesis:{hypothesis}\nLabel:'},
+        ]
+    print((pipe(messages, **generation_args)[0]))
+    return (pipe(messages, **generation_args)[0]['generated_text'])
+
+def main():
+    parser  = argparse.ArgumentParser()
+    parser.add_argument('--input-path',type=str)
+    parser.add_argument('--example-path',type=str)
+    parser.add_argument('--output-path',type=str)
+    parser.add_argument('--shot', type=int, default=4, help="In-Context의 개수")
+    args = parser.parse_args()
+    
+    data=[]
+    new_li=[]
+    
+    with open(args.input_path,'r',encoding='utf-8') as f:
+        for line in f:
+            try:
+                d = json.loads(line)
+                data.append(d)
+            except json.JSONDecodeError as e:
+                print(f"Error parsing JSON: {e}")
+                
+                
+    with open(args.output_path,'w') as out_f:
+        for d in data:
+            empty={
+                "premise": "",
+                "hypothesis": "",
+                "label": "",
+                "Label":""
+            }
+            
+            empty['premise'] = d['premise']
+            empty['hypothesis'] = d['hypothesis']
+            empty['label'] = d['label']
+            
+            Label = response(args,d['premise'], d['hypothesis'])
+            empty['Label'] = Label
+            new_li.append(empty)
+            out_f.write(json.dumps(empty) + '\n' )
+    
+if __name__ == '__main__':
+    main()
---
+++ Script/Phi3/CoT/4shot/run_CoT.sh
@@ -0,0 +1,4 @@
+python3.10 /home/hhs/main/llama3/chr/phi3/CoT/4shot/CoT.py \
+    --input-path /home/hhs/main/llama3/chr/Data/original_input.json \
+    --example-path /home/hhs/main/llama3/chr/phi3/CoT/4shot/CoT_prompt.txt \
+    --output-path ./again_Phi3_4K_CoT.json\
